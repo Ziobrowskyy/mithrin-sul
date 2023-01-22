720a12b546778d91aa4ec2ecbfb75c03d3f912a0
Ziobrowskyy <thomas.ziobrowski@gmail.com>
2023-01-22 15:40:42 +0100

    Supabase client configuration

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "picsum.photos",
-            }
-        ]
-    }
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos"
+      },
+      {
+        protocol: "https",
+        hostname: process.env.SUPBASE_HOSTNAME
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
