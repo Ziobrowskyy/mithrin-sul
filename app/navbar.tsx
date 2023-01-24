8dd4a216c85f4fc57ac249ad2873c964d17fb76c
Ziobrowskyy <thomas.ziobrowski@gmail.com>
2023-01-24 18:32:43 +0100

    Changes navbar layout

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -9,17 +9,16 @@ export default function Navbar() {
   ]
   return (
     <header className={"z-20 sticky top-0 backdrop-blur"}>
-      <nav className={"mx-auto flex flex-row items-center px-1 py-2 max-w-3xl"}>
+      <nav className={"mx-auto max-w-3xl flex content-around items-center px-1 py-2"}>
         <Link href={"/"} className={"flex-initial"}>
           <Image
-            className={"max-h-16"}
             src={"/logo.svg"}
+            width={64}
+            height={64}
             alt={"logo"}
-            width={200}
-            height={40}
           />
         </Link>
-        <ul className={"flex-1 justify-around flex h-10 gap-6"}>
+        <ul className={"justify-around flex h-10 gap-6"}>
           {menuItems.map(({ href, name }, index) => (
             <li
               key={index}
