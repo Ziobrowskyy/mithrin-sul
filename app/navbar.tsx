a461d682bebd26fe070133ca3d1fe50794e9e825
Ziobrowskyy <thomas.ziobrowski@gmail.com>
2023-01-24 23:23:58 +0100

    Add layout feeling to cramped and add new links

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,29 +2,37 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Navbar() {
-  const menuItems = [
-    { name: "Add post", href: "/add-post" },
-    { name: "Menu link 2", href: "/menu-2" },
-    { name: "Menu link 3", href: "/menu-3" },
+  const menuItems: {name: string, href: string}[] = [
+    { name: "about us", href: "/about-us" },
+    { name: "our dogs", href: "/dogs" },
+    { name: "contact", href: "/contact" },
+    { name: "add post", href: "/add-post" },
+    { name: "edit posts", href: "/edit-posts" },
   ]
   return (
-    <header className={"z-20 sticky top-0 backdrop-blur"}>
-      <nav className={"mx-auto max-w-3xl flex content-around items-center px-1 py-2"}>
+    <header className={"z-20 sticky top-0 bg-white/70 backdrop-blur"}>
+      <nav className={"mx-auto max-w-3xl flex gap-6 content-around items-center px-1 py-2"}>
         <Link href={"/"} className={"flex-initial"}>
           <Image
             src={"/logo.svg"}
+            loading={"eager"}
             width={64}
             height={64}
             alt={"logo"}
           />
         </Link>
-        <ul className={"justify-around flex h-10 gap-6"}>
+        <ul className={"flex h-10 gap-6"}>
           {menuItems.map(({ href, name }, index) => (
             <li
-              key={index}
               className={"flex transition hover:text-pink-400 hover:underline justify-center items-center"}
+              key={index}
             >
-              <Link href={href}>{name}</Link>
+              <Link
+                className={"capitalize"}
+                href={href}
+              >
+                {name}
+              </Link>
             </li>
           ))}
         </ul>
