78b9486a656f2d462001e1e2718cc45a167fe8e5
Ziobrowskyy <thomas.ziobrowski@gmail.com>
2023-01-24 18:18:52 +0100

    Changes in post's layout and add horizontal row

--- app/post.tsx
+++ app/post.tsx
@@ -17,10 +17,10 @@ export default function PostComponent({
   const files = post.files.map(url => ({ url }));
   return (
     <div
-      className={`p-4 max-w-3xl bg-white border-gray-200 hover:border-pink-200 border rounded ${className ?? ""}`}>
+      className={`group/post p-4 w-full bg-white border-gray-200 focus-within:border-pink-200 hover:border-pink-200 border rounded ${className ?? ""}`}>
       <div className={"flex justify-between items-center gap-2"}>
         <h2 className={"font-medium text-gray-800 text-2xl"}>{post.title}</h2>
-        <div className={" gap-1 font-light text-gray-600 tracking-wider uppercase"}>
+        <div className={"gap-1 font-light text-gray-600 tracking-wider uppercase"}>
           <span>{parsedDate}{hasLocation ? ", " : ""}</span>
           {hasLocation && (
             <span>
@@ -32,11 +32,10 @@ export default function PostComponent({
       {files.length > 0 &&
         <ImageCarousel className={"mt-4"} images={files}/>
       }
-      <div>
-        <p className={"py-4 text-gray-600 "}>
-          {post.content}
-        </p>
-      </div>
+      <hr className={"my-4 group-focus-within/post:border-pink-200 group-hover/post:border-pink-200"}/>
+      <p className={"text-gray-600 mb-2"}>
+        {post.content}
+      </p>
     </div>
   )
 }
